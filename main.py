0e1047f57dedfed22d71ec2cc58bca952effe843
pafufodon@gmail.com <pafufodon@gmail.com>
2022-08-24 11:58:26 -0300

    v1.0

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import currency
 # import api_bot
 
 # Bot
-bot = tb.TeleBot(KEY_BOT_HEROKU)  # Heroku Config Vars ENV['S3_KEY']
+bot = tb.TeleBot(KEY_BOT_HEROKU2)  # Heroku Config Vars ENV['S3_KEY']
 # bot = tb.TeleBot(api_bot.api_bot)  # Pycharm local
 
 logger = tb.logger
