f689fcb02900c2150769e393e81c27474b65b635
pafufodon@gmail.com <pafufodon@gmail.com>
2022-08-24 16:13:31 -0300

    v1.1

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def content_types(message):
 
 
 # Cotação do dólar
-@bot.message_handler(regexp="dolar")
+@bot.message_handler(regexp="dolar|dólar")
 def currency2(message):
     currency_dic = json.loads(convert('usd', 'brl', 1))  # json to dic
     resultado_currency = currency_dic['amount'].replace(".", ",")
