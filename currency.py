b4e2c5fe7bc6a6c5ccc3a972ec19fdeb0f3dfff7
pafufodon@gmail.com <pafufodon@gmail.com>
2022-08-24 13:25:03 -0300

    v1.1

--- currency.py
+++ currency.py
@@ -2,10 +2,10 @@ import json
 from google_currency import convert
 
 
-def googlecurrency(moeda1, moeda2):
+def moedacheck(moeda1, moeda2):
     currency_dic = json.loads(convert(f'{moeda1}', f'{moeda2}', 1))  # json to dic
     resultado_currency = currency_dic['amount'].replace(".", ",")
-    # print(f'R${resultado_currency}')
+    print(f'R${resultado_currency}')
 
 
-
+moedacheck('usd', 'brl')
--- main.py
+++ main.py
@@ -22,8 +22,7 @@ def content_types(message):
 # Por palavra enviada
 @bot.message_handler(regexp="dolar")
 def currency(message):
-    currency.googlecurrency("usd", "brl")
-    bot.reply_to(message.chat.id, f'Cotação atual do dólar: {resultado_currency}.')
+    bot.reply_to(message.chat.id, f'Cotação atual do dólar: {currency.moedacheck("usd", "brl")}.')
 
 
 @bot.message_handler(regexp="infos")
