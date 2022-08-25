e93bef4276cfd028b5fbddd4f2f4e78360acdf4a
pafufodon@gmail.com <pafufodon@gmail.com>
2022-08-25 14:06:31 -0300

    v1.1

--- main.py
+++ main.py
@@ -153,7 +153,8 @@ def handle_deia(message):
 def step_Set_Ideia(message):
     ideiatexto = message.text
     id_criador = 1317880277  # Meu ID.
-    bot.send_message(id_criador, f'Ideia enviada pelo {message.from_user.username}({message.from_user.first_name}), ID: {message.from_user.id}:\n{ideiatexto}')
+    bot.send_message(id_criador, f'Ideia enviada pelo {message.from_user.username}({message.from_user.first_name}),'
+                                 f' ID: {message.from_user.id}:\n{ideiatexto}')
     bot.send_message(message.chat.id, 'Obrigado, sua ideia foi enviada!')
 
 
